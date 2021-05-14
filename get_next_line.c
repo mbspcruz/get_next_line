49a6befcdf4b29a3824a94f0f013fd57a1fac772
Mariana <mda-cruz@dadas-imac.lan>
2021-05-14 14:37:12 +0100

    Need to fix leaks

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mda-cruz <user@student.42lisboa.com>       +#+  +:+       +#+        */
+/*   By: mda-cruz <mda-cruz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 16:05:21 by david             #+#    #+#             */
-/*   Updated: 2021/05/13 16:30:31 by mda-cruz         ###   ########.fr       */
+/*   Updated: 2021/05/14 14:26:09 by mda-cruz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,15 @@ char    *ft_line_bf_n(char *line)
     int i;
     char *tmp;
 
+    if (!line)
+        return (0);
     i = 0;
     while(line && line[i] && line[i] != '\n')
         i++;
     if(!(tmp = (char *)malloc(sizeof(char) * (i + 1))))
         return (0);
-    while(line && line[i] != '\n' && line[i])
+    i = 0;
+    while(line && line[i] && line[i] != '\n')
     {
         tmp[i] = line[i];
         i++;
@@ -44,6 +47,8 @@ char    *ft_line_af_n(char *line)
     char *sv;
 	int b;
     
+    if (!line)
+        return (0);
     i = 0;
     b = 0;
     while(line[i] && line[i] != '\n')
@@ -60,23 +65,23 @@ char    *ft_line_af_n(char *line)
     return (sv);
 }
 
-int ft_check_nl(char *line)
-{
-    int i;
+// int ft_check_nl(char *line)
+// {
+//     int i;
 
-    i = 0;
-    //printf("*line: %s", line);
-    // printf("line[i]: %c", line[i]);
-    while (line && line[i])
-    {
-        if (line[i] == '\n')
-        {
-            return(1);
-        }
-        i++;
-    }
-    return (0);
-}
+//     i = 0;
+//     //printf("*line: %s", line);
+//     // printf("line[i]: %c", line[i]);
+//     while (line && line[i])
+//     {
+//         if (line[i] == '\n')
+//         {
+//             return(1);
+//         }
+//         i++;
+//     }
+//     return (0);
+// }
 
 int get_next_line(int fd, char **line)
 {
@@ -84,25 +89,45 @@ int get_next_line(int fd, char **line)
     int rd;
     char *buf;
     
-    
-    if(!(buf = (char *)malloc(sizeof(char) * BUF_SIZE +1)) || BUF_SIZE <= 0 || fd < 0 || line == 0)
-        return (-1);
-    
+    buf = (char *)malloc(sizeof(char) * BUF_SIZE + 1);
     while((rd = read(fd, buf, BUF_SIZE)) > 0)
     {
         buf[rd] = '\0';
         save[fd] = ft_strjoin(save[fd], buf);
-        if (ft_strchr(save[fd], '\n'))
-            break; 
+        if (ft_strchr(buf, '\n'))
+            break;
     }
     free(buf);
     *line = ft_line_bf_n(save[fd]);
     save[fd] = ft_line_af_n(save[fd]);
-    if (rd == 0)
+
+    if (!*line || rd < 0)
+        return (-1);
+    else if (rd == 0)
         return (0);
     else
         return (1);
 }
+
+// int main(void)
+// {
+// 	char *line;
+// 	int fd;
+// 	int fd2;
+// 	int	counter;
+
+// 	fd = open("txt.txt", O_RDONLY);
+// 	fd2 = open("txt2.txt", O_RDONLY);
+// 	while(get_next_line(fd, &line))
+// 	{
+// 		printf("%s\n\n", line);
+// 	}
+// 	while(get_next_line(fd2, &line))
+// 	{
+// 		printf("%s\n\n", line);
+// 	}
+		
+// }
 
 /*int main()
 {
